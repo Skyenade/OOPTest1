25d87211280af042334c775ab3555da1ffce6f5d
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-21 23:01:05 -0500

    2nd commit

--- src/Pack_Test_1/Demo.java
+++ src/Pack_Test_1/Demo.java
@@ -1,7 +1,6 @@
 package Pack_Test_1;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Demo {
 
@@ -12,13 +11,13 @@ public class Demo {
 		System.out.println("Hello. Welcome to the store");
 		System.out.println("Please give me the inventory amount: ");
 		int maxComputer = kb.nextInt();
-		
-		Computer [] inventory = new Computer [maxComputer];
-		
+
+		Computer[] inventory = new Computer[maxComputer];
+
 		// print the menu inside the do-while
-		
+
 		int choice;
-				
+
 		do {
 			System.out.println("What do you want to do? ");
 			System.out.println("1.Enter new computers (password required)");
@@ -28,39 +27,135 @@ public class Demo {
 			System.out.println("5.Quit");
 			System.out.println("Please enter your choice >");
 			choice = kb.nextInt();
-		} while (choice != 5);
-		
-		
-		
-		System.out.println("Please enter the password: ");
-		String inputPassword = kb.next();
-		String password = passwordChecker(inputPassword);
 
-		
-		
+			switch (choice) {
+			case 1:
+				System.out.println("Please input your password: ");
+				String constantPassword = "password";
+				int tryCount = 0;
+				boolean passwordChecker = false;
 
+				do {
+					String enteredPassword = kb.next();
+					if (enteredPassword.equals(constantPassword)) {
+						passwordChecker = true;
+						break;
+					} else {
+						tryCount++;
+						System.out.println("Incorrect password. Try again. There are " + (3 - tryCount) + "remaining ");
+					}
+					if (tryCount == 4) {
+						break;
+					}
+				} while (tryCount < 4);
 
-			
-		
-	}
-	
-	public static String passwordChecker(String inputPassword) {
-		int tries = 3;
-		int tryCount = 0;
-		String pass = inputPassword;
-		while (tryCount <= tries) {
-			if (inputPassword != "password") {
-				tryCount++;	
+				if (passwordChecker) {
+					System.out.println("How many computers do you want to enter: ");
+					int compCount = kb.nextInt();
+					boolean hasEmptySpace = false;
+					int filledCount = 0;
+					int arrayPlace = -1;
+					int len = inventory.length;
+					for (int i = 0; i < len; i++) {
+						if (inventory[i] == null) {
+							hasEmptySpace = true;
+							arrayPlace = i;
+							break;
+						} else {
+							filledCount++;
+						}
+					}
+
+					if (hasEmptySpace) {
+						if (compCount <= len) {
+							System.out.println("There are " + (len - filledCount) + " empty spaces in the inventory.");
+							for (int i = arrayPlace; i < compCount; i++) {
+								System.out.println("Please enter the information from the computer: ");
+								System.out.println("what's the brand? ");
+								String b = kb.next();
+								System.out.println("what's the model? ");
+								String m = kb.next();
+								System.out.println("what's the price? ");
+								double p = kb.nextDouble();
+								inventory[arrayPlace + filledCount + i] = new Computer(b, m, p);
+							}
+							filledCount += compCount;
+						} else {
+							System.out.println("Not enough space in the inventory.");
+						}
+					}
+				}
+
+			case 2:
+				System.out.println("Please input your password: ");
+				constantPassword = "password";
+				tryCount = 0;
+				passwordChecker = false;
+
+				do {
+					String enteredPassword = kb.next();
+					if (enteredPassword.equals(constantPassword)) {
+						passwordChecker = true;
+						break;
+					} else {
+						tryCount++;
+						System.out.println("Incorrect password. Try again. There are " + (3 - tryCount) + "remaining ");
+					}
+					if (tryCount == 4) {
+						break;
+					}
+				} while (tryCount < 4);
+
+				
+				System.out.println("Which computer would you like to update? ");
+				boolean indexEmpty = false;
+				
+				while (!indexEmpty) {
+					int compNum = kb.nextInt();
+					int arrayPlace = compNum - 1;
+					if (inventory[arrayPlace] != null) {
+						
+					}
+					
+				}
+				
+				
+				
+				
+				
+				
+				System.out.println("Which computer would you like to update? ");
+				
+				
+				
+				System.out.println("What's the new brand? ");
+				String newB = kb.next();
+				inventory[arrayPlace].setBrand(newB);
+				System.out.println("What's the new model? ");
+				String newM = kb.next();
+				inventory[arrayPlace].setModel(newM);
+				
+				
 			}
-			
-		}		
-		return pass;
+
+		} while (choice != 5);
+
+		kb.close();
+
+//	public static String passwordChecker(String inputPassword) {
+//		int tryCount = 0;
+//		String checkedPass = inputPassword;
+//		while (tryCount <= 3) {
+//			if (inputPassword != "password") {
+//				tryCount++;	
+//			}
+//			
+//		}		
+//		return checkedPass;
+//	}
 	}
-	
 
 }
-
-
 
 // show in go to the project, not the package, before src
 // copy the path and cd to the path
